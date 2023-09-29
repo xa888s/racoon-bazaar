e574ca10beada2b65e92ac5fb86144d6478e4479
edengler01 <erik_dengler@sfu.ca>
2023-09-28 23:02:25 -0700

    Doc + added server insert
    Added some documentation and created an insertOrder that will send an insert statement from the server to the database

--- backend/database.js
+++ backend/database.js
@@ -1,3 +1,6 @@
+//js file where database queries will be made
+
+
 import mysql from 'mysql2'
 //import dotenv from 'dotenv'
 //dotenv.config()
@@ -15,11 +18,21 @@ const pool = mysql.createPool({
     database:'sql3649600'
 }).promise()
 
+
 export async function getSells(){
     const [result] = await pool.query("SELECT * FROM MarketplaceSell")
     return result;
 }
 
+export async function createSale(buyerID, name, author, edition, course ){
+    const [result] = await pool.query('INSERT INTO MarketPlaceSell(buyerID,name,author, edition, course) VALUES (?,?,?,?)', [buyerID, name, author, edition, course])
+    return result.insertId;
+
+}
+
+
+
+/*
 const sells = await getSells()
 console.log(sells)
-
+*/
--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,46 @@
+//Server file where certain behaviour will happen
+
+
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 import bodyParser from "body-parser";
 //local functions from database.js
 import {getSells} from './database.js'
+import { emitWarning } from "process";
 //to fix __dirname errors
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
 
+app.use(express.json);
+/*
+Anything in the '' determines what the "function" happens
+'/' is the default case aka the index html
 
+*/
 app.get('/', async (req,res)=>{
     console.log("Server running");
     const sells = await getSells();
     res.send(sells);
 });
+
+
+//function to get all the sale orders on the database
+app.get('/retrieveSales',async(req,res)=>{
+    console.log("Retrieve sells from database");
+})
+
+
+//function to insert into database
+app.post('/insertOrder', async(req,res)=>
+{  
+    const {buyerID, name, author, edition, course} = req.body;
+    const order = await createSale(buyerID, name, author, edition, course);
+    
+    console.log("Inserting an order");
+})
+
+console.log("Server is running");
 
 app.listen(3000);
